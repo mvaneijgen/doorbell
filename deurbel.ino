eb546f97906645da192601667d4ce15d6302497d
Mitchel van Eijgen <mvaneijgen@gmail.com>
2024-08-22 22:05:48 +0200

    Add current time and deboucne

--- deurbel.ino
+++ deurbel.ino
@@ -1,6 +1,7 @@
 /*
   Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/esp32-pushover-notifications-arduino/
+  Complete project details at
+  https://RandomNerdTutorials.com/esp32-pushover-notifications-arduino/
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files.
@@ -9,53 +10,58 @@
   copies or substantial portions of the Software.
 */
 #include "config.h"
-#include <WiFi.h>
-#include <HTTPClient.h>
-#include <WiFiClientSecure.h>
 #include <ArduinoJson.h>
+#include <HTTPClient.h>
+#include <NTPClient.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <WiFiUdp.h>
 
-const int doorbell = 34;         // current sensor connected to pin 34 and ground
-int senseDoorbell = 0;           // variable to hold doorbell sensor reading
-int debounce = 5000;             // only allow one DingDong per second
-unsigned long currentMillis = 0; // how many milliseconds since the Arduino booted
-unsigned long prevRing = 0;      // current sensor connected to pin 34 and ground
+const int doorbell = 34; // current sensor connected to pin 34 and ground
+int senseDoorbell = 0;   // variable to hold doorbell sensor reading
+unsigned long previousMillis =
+    0;                       // Store the last time the notification was sent
+const long interval = 10000; // Interval between notifications (in milliseconds)
+unsigned long prevRing = 0;  // current sensor connected to pin 34 and ground
 int LED_BUILTIN = 2;
 
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", 0, 60000); // NTP client setup
 //--------------------------------//
 // Pushover Setup
 //--------------------------------//
 const char *pushoverApiEndpoint = "https://api.pushover.net/1/messages.json";
 
 // Pushover root certificate (valid from 11/10/2006 to 15/01/2038)
-const char *PUSHOVER_ROOT_CA = "-----BEGIN CERTIFICATE-----\n"
-                               "MIIDjjCCAnagAwIBAgIQAzrx5qcRqaC7KGSxHQn65TANBgkqhkiG9w0BAQsFADBh\n"
-                               "MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n"
-                               "d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBH\n"
-                               "MjAeFw0xMzA4MDExMjAwMDBaFw0zODAxMTUxMjAwMDBaMGExCzAJBgNVBAYTAlVT\n"
-                               "MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5j\n"
-                               "b20xIDAeBgNVBAMTF0RpZ2lDZXJ0IEdsb2JhbCBSb290IEcyMIIBIjANBgkqhkiG\n"
-                               "9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuzfNNNx7a8myaJCtSnX/RrohCgiN9RlUyfuI\n"
-                               "2/Ou8jqJkTx65qsGGmvPrC3oXgkkRLpimn7Wo6h+4FR1IAWsULecYxpsMNzaHxmx\n"
-                               "1x7e/dfgy5SDN67sH0NO3Xss0r0upS/kqbitOtSZpLYl6ZtrAGCSYP9PIUkY92eQ\n"
-                               "q2EGnI/yuum06ZIya7XzV+hdG82MHauVBJVJ8zUtluNJbd134/tJS7SsVQepj5Wz\n"
-                               "tCO7TG1F8PapspUwtP1MVYwnSlcUfIKdzXOS0xZKBgyMUNGPHgm+F6HmIcr9g+UQ\n"
-                               "vIOlCsRnKPZzFBQ9RnbDhxSJITRNrw9FDKZJobq7nMWxM4MphQIDAQABo0IwQDAP\n"
-                               "BgNVHRMBAf8EBTADAQH/MA4GA1UdDwEB/wQEAwIBhjAdBgNVHQ4EFgQUTiJUIBiV\n"
-                               "5uNu5g/6+rkS7QYXjzkwDQYJKoZIhvcNAQELBQADggEBAGBnKJRvDkhj6zHd6mcY\n"
-                               "1Yl9PMWLSn/pvtsrF9+wX3N3KjITOYFnQoQj8kVnNeyIv/iPsGEMNKSuIEyExtv4\n"
-                               "NeF22d+mQrvHRAiGfzZ0JFrabA0UWTW98kndth/Jsw1HKj2ZL7tcu7XUIOGZX1NG\n"
-                               "Fdtom/DzMNU+MeKNhJ7jitralj41E6Vf8PlwUHBHQRFXGU7Aj64GxJUTFy8bJZ91\n"
-                               "8rGOmaFvE7FBcf6IKshPECBV1/MUReXgRPTqh5Uykw7+U0b6LJ3/iyK5S9kJRaTe\n"
-                               "pLiaWN0bfVKfjllDiIGknibVb63dDcY3fe0Dkhvld1927jyNxF1WW6LZZm6zNTfl\n"
-                               "MrY=\n"
-                               "-----END CERTIFICATE-----\n";
+const char *PUSHOVER_ROOT_CA =
+    "-----BEGIN CERTIFICATE-----\n"
+    "MIIDjjCCAnagAwIBAgIQAzrx5qcRqaC7KGSxHQn65TANBgkqhkiG9w0BAQsFADBh\n"
+    "MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n"
+    "d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBH\n"
+    "MjAeFw0xMzA4MDExMjAwMDBaFw0zODAxMTUxMjAwMDBaMGExCzAJBgNVBAYTAlVT\n"
+    "MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5j\n"
+    "b20xIDAeBgNVBAMTF0RpZ2lDZXJ0IEdsb2JhbCBSb290IEcyMIIBIjANBgkqhkiG\n"
+    "9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuzfNNNx7a8myaJCtSnX/RrohCgiN9RlUyfuI\n"
+    "2/Ou8jqJkTx65qsGGmvPrC3oXgkkRLpimn7Wo6h+4FR1IAWsULecYxpsMNzaHxmx\n"
+    "1x7e/dfgy5SDN67sH0NO3Xss0r0upS/kqbitOtSZpLYl6ZtrAGCSYP9PIUkY92eQ\n"
+    "q2EGnI/yuum06ZIya7XzV+hdG82MHauVBJVJ8zUtluNJbd134/tJS7SsVQepj5Wz\n"
+    "tCO7TG1F8PapspUwtP1MVYwnSlcUfIKdzXOS0xZKBgyMUNGPHgm+F6HmIcr9g+UQ\n"
+    "vIOlCsRnKPZzFBQ9RnbDhxSJITRNrw9FDKZJobq7nMWxM4MphQIDAQABo0IwQDAP\n"
+    "BgNVHRMBAf8EBTADAQH/MA4GA1UdDwEB/wQEAwIBhjAdBgNVHQ4EFgQUTiJUIBiV\n"
+    "5uNu5g/6+rkS7QYXjzkwDQYJKoZIhvcNAQELBQADggEBAGBnKJRvDkhj6zHd6mcY\n"
+    "1Yl9PMWLSn/pvtsrF9+wX3N3KjITOYFnQoQj8kVnNeyIv/iPsGEMNKSuIEyExtv4\n"
+    "NeF22d+mQrvHRAiGfzZ0JFrabA0UWTW98kndth/Jsw1HKj2ZL7tcu7XUIOGZX1NG\n"
+    "Fdtom/DzMNU+MeKNhJ7jitralj41E6Vf8PlwUHBHQRFXGU7Aj64GxJUTFy8bJZ91\n"
+    "8rGOmaFvE7FBcf6IKshPECBV1/MUReXgRPTqh5Uykw7+U0b6LJ3/iyK5S9kJRaTe\n"
+    "pLiaWN0bfVKfjllDiIGknibVb63dDcY3fe0Dkhvld1927jyNxF1WW6LZZm6zNTfl\n"
+    "MrY=\n"
+    "-----END CERTIFICATE-----\n";
 // END Pushover Setup --------------//
 
 //--------------------------------//
 // Send push notification function
 //--------------------------------//
-void sendPushoverNotification(const char *title, const char *message)
-{
+void sendPushoverNotification(const char *title, const char *message) {
   // Create a JSON object with notification details
   StaticJsonDocument<512> notification;
   notification["token"] = apiToken;  // required
@@ -91,15 +97,12 @@ void sendPushoverNotification(const char *title, const char *message)
   int httpResponseCode = https.POST(jsonStringNotification);
 
   // Check the response
-  if (httpResponseCode > 0)
-  {
+  if (httpResponseCode > 0) {
     Serial.printf("HTTP response code: %d\n", httpResponseCode);
     String response = https.getString();
     Serial.println("Response:");
     Serial.println(response);
-  }
-  else
-  {
+  } else {
     Serial.printf("HTTP response code: %d\n", httpResponseCode);
   }
 
@@ -108,14 +111,12 @@ void sendPushoverNotification(const char *title, const char *message)
 }
 // END Send push notification function --------------//
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
 
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED)
-  {
+  while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
     Serial.println("Connecting to WiFi...");
@@ -126,23 +127,25 @@ void setup()
   digitalWrite(LED_BUILTIN, LOW);
 
   // Make HTTPS POST request to send notification
-  if (WiFi.status() == WL_CONNECTED)
-  {
+  if (WiFi.status() == WL_CONNECTED) {
     sendPushoverNotification("Doorbell setup", "Connected to Wi-Fi");
   }
+  timeClient.begin();
 }
 
-void loop()
-{
-  // currentMillis = millis();
-  // only check the doorbell if it hasn't been hit in the last second
-  // if (currentMillis - prevRing >= debounce)
-  // {
-  senseDoorbell = analogRead(doorbell); // read the doorbell sensor
-  if (senseDoorbell > 50)
-  {
-    sendPushoverNotification("Doorbell rang", "Get current time");
+void loop() {
+  timeClient.update(); // Update the NTP client to get the latest time
+  unsigned long currentMillis = millis(); // Get the current time
+
+  senseDoorbell = analogRead(doorbell); // Read the doorbell sensor
+
+  if (senseDoorbell > 50 && currentMillis - previousMillis >= interval) {
+    String currentTime =
+        timeClient
+            .getFormattedTime(); // Get the current time as a formatted string
+
+    sendPushoverNotification("Doorbell rang", currentTime.c_str());
+    previousMillis =
+        currentMillis; // Update the last time the notification was sent
   }
-  prevRing = currentMillis; // engage debounce mode!
-  // }
 }
